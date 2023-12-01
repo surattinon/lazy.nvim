a72ba3a29fd3a14d73153e3c4a2f31b1db805342
surattinon <surattinon.basu@gmail.com>
2023-12-01 11:28:47 +0700

    update config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,40 +1,56 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local discipline = require("custom.discipline")
+
+discipline.cowboy()
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
--- Increase / Decrease
+keymap.set("n", "x", '"_x')
+
+-- Increment/decrement
 keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
 
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
 -- Select all
-keymap.set("n", "<C-a", "gg<S-v>G")
+keymap.set("n", "<C-a>", "gg<S-v>G")
 
--- jump list
-keymap.set("n", "<C-m", "<C-i>", opts)
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
--- Tab control
-keymap.set("n", "te", "tabedit", opts)
-keymap.set("n", "<tab>", "tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", "tabprev<Return>", opts)
+-- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
 
--- split window
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 -- Move window
 keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sl", "<C-w>l")
-keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
 -- Resize window
 keymap.set("n", "<C-w><left>", "<C-w><")
 keymap.set("n", "<C-w><right>", "<C-w>>")
 keymap.set("n", "<C-w><up>", "<C-w>+")
 keymap.set("n", "<C-w><down>", "<C-w>-")
 
--- Diagnostic
+-- Diagnostics
 keymap.set("n", "<C-j>", function()
   vim.diagnostic.goto_next()
 end, opts)
+
+keymap.set("n", "<leader>r", function()
+  require("craftzdog.utils").replaceHexWithHSL()
+end)
